f0d626faa38de8bd1f69eecf5934d04fcc983e0c
wiktorklusek <63818570+wiktorklusek@users.noreply.github.com>
2023-02-05 21:41:18 +0000

    Update app.js

--- Client/app.js
+++ Client/app.js
@@ -131,7 +131,7 @@ let webstore = new Vue({
       };
 
 fetch(
-  `https://cst3145-wk186.herokuapp.com/collections/orders`,
+  'https://cst3145-wk186.herokuapp.com/collections/orders',
   {
     method: "POST",
     headers: {
