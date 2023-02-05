d3b0cbcc75eab978f7107f11f7febc79d7357194
wiktorklusek <63818570+wiktorklusek@users.noreply.github.com>
2023-02-05 16:23:42 +0000

    Update app.js
    changing search?q=%{this.search} to search?q={this.search}

--- Client/app.js
+++ Client/app.js
@@ -56,7 +56,7 @@ let webstore = new Vue({
             
       //Searching functionality implemented in API
       search() {
-          fetch(`https://cst3145-wk186.herokuapp.com/collections/products/search?q=%{this.search}`)
+          fetch(`https://cst3145-wk186.herokuapp.com/collections/products/search?q={this.search}`)
                .then(response => response.json())
                .then(data => {
                   this.searchAPIResults = data;
