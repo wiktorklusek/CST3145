5c54feb34f19023345f9b5419ff927013374ddec
wiktorklusek <63818570+wiktorklusek@users.noreply.github.com>
2023-02-07 15:09:44 +0000

    FORMATTED CODE

--- Client/app.js
+++ Client/app.js
@@ -8,7 +8,7 @@ let webstore = new Vue({
     cart: [],
     ascending: true,
     // sortBy: 'subject',
-    search: '',
+    search: "",
     searchValue: "",
 
     // Initial API functionality for search
@@ -56,25 +56,31 @@ let webstore = new Vue({
   methods: {
     //Searching functionality implemented in API
     searchAPI() {
-      fetch(`https://cst3145-wk186.herokuapp.com/collections/products/search?q=${this.search}`).then(
-          (response) => response.json())
-            .then((data) => {
-              this.searchResults = data;
-            webstore.products = json;
-            console.log(data);
+      fetch(
+        `https://cst3145-wk186.herokuapp.com/collections/products/search?q=${this.search}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          this.searchResults = data;
+          webstore.products = json;
+          console.log(data);
         })
         .catch((error) => console.error(error));
     },
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     addClass(product) {
       this.cart.push(product.id);
     },
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     showCheckout() {
       this.showProduct = this.showProduct ? false : true;
     },
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     canAddClass(product) {
       return product.numberOfSpaces > this.cartCount(product.id);
     },
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     cartCount(id) {
       let count = 0;
       for (let i = 0; i < this.cart.length; i++) {
@@ -84,7 +90,7 @@ let webstore = new Vue({
       }
       return count;
     },
-
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     submitForm() {
       // const formIsValid = this.firstNameIsValid // && this.lastNameIsValid ...etc
 
@@ -98,7 +104,7 @@ let webstore = new Vue({
 
       // alert('Order submitted!')
     },
-
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // removeFromCart method
     removeFromCart(product) {
       let index = this.cart.indexOf(product.id);
@@ -107,7 +113,7 @@ let webstore = new Vue({
       }
       product.numberOfSpaces + 1; // here was the code that simply was adding two spaces instead of one, previously deleted
     },
-
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     // showInCart method
     showInCart(product, id) {
       // very javascripti :)
@@ -116,114 +122,53 @@ let webstore = new Vue({
         return product.name;
       }
     },
-
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //Submitting an order, updating the available spaces for the products submitted
-submitCheckoutForm() {
-      
-    this.cart.forEach((i) => {
-              const newOrder = {
-                          name: this.order.firstName,
-                          numberOfSpaces: this.orderLessonSpaces,
-                          id: this.lessonsIDs,
-                          phoneNumber: this.order.phoneNumber
-          };
-          this.postOrder(newOrder);
-              
-          // update available lesson space with put
-    var formattedLesson = { numberOfSpaces: i.numberOfSpaces }
-          // ...
-          
-          });
-          this.cart = [];
-    this.togglePage();
-    // dialog confirmation
-    showConfirmationDialog() {
-        alert('Order has been submitted successfully')
-      }
-  },
-												
-												
-			/// A fetch that saves a new order with POST
-      postOrder(jsonData) {
-            fetch(`https://cst3145-wk186.herokuapp.com/collections/orders`, {
-                method: "POST",
-                body: JSON.stringify(jsonData),
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            }).then(response => response.json())
-                .then(responseData => {
-                    console.log(responseData);
-                })
-                .catch(error => {
-                    console.log(error);
-                });
+    submitCheckoutForm() {
+      this.cart.forEach((i) => {
+        const newOrder = {
+          name: this.order.firstName,
+          numberOfSpaces: this.orderLessonSpaces,
+          id: this.lessonsIDs,
+          phoneNumber: this.order.phoneNumber,
+        };
+        this.postOrder(newOrder);
+
+        // update available lesson space with put
+        var formattedLesson = { numberOfSpaces: i.numberOfSpaces };
+        // ...
+      });
+    },
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /// A fetch that saves a new order with POST
+    postOrder(jsonData) {
+      fetch(`https://cst3145-wk186.herokuapp.com/collections/orders`, {
+        method: "POST",
+        body: JSON.stringify(jsonData),
+        headers: {
+          "Content-Type": "application/json",
         },
-      
-//     this.computeLessonsForOrder();
-      
-//     const newOrder = {
-//       name: this.order.firstName,
-//       numberOfSpaces: this.orderLessonSpaces,
-//       id: this.lessonsIDs,
-//       phoneNumber: this.order.phoneNumber
-//     }
+      })
+        .then((response) => response.json())
+        .then((responseData) => {
+          console.log(responseData);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
 
-//   fetch("https://cst3145-wk186.herokuapp.com/collections/orders", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify(newOrder)
-//   }).then(
-//       function (response) {
-//           response.json().then(
-//               function (json) {
-//                   console.log("Success: " + json.ackowledged);
-//               })
-//       })
-
-//   //PUT route for updating the lessons    
-//   this.cart.forEach(j => {
-//     this.product.forEach(i => {
-//       let count = 0;
-//       if (j === i.id) {
-//         count++;
-//         const updateProduct = {
-//           numberOfSpaces: i.numberOfSpaces - count
-//         }
-
-//         fetch(`https://cst3145-wk186.herokuapp.com/collections/products/${i._id}`, {
-//           method: "PUT",
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           body: JSON.stringify(updateProduct)
-//         }).then(
-//                   function (response) {
-//                       response.json().then(
-//                           function (json) {
-//                               console.log("Success: " + json.ackowledged);
-//                           }
-//                       )}
-//               )}
-//       })
-//   })
-//   alert("Thank you for submitting your order!");
-//},
     //Input validation methods
     validName(firstName) {
       var name_regex = /^[a-zA-Z]+$/;
       return name_regex.test(firstName);
     },
+
     validNumber(phoneNumber) {
       var phone_regex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
       return phone_regex.test(phoneNumber);
     },
-    // //Cart verifier
-    // canAddToCart(lesson) {
-    //   return lesson.availableSpaces > this.cartCount(lesson.id);
-    // },
+
     //Cart count method
     cartCount(id) {
       let count = 0;
@@ -234,14 +179,17 @@ submitCheckoutForm() {
       }
       return count;
     },
+
     //Classes left computation
     classesLeft(product) {
       return product.numberOfSpaces - this.cartCount(product.id);
     },
+
     //Removing lessons from the checkout
     removeLesson(index) {
       this.cart.splice(index, 1);
     },
+
     //Sorting method
     sort(s) {
       if (s === this.sortBy) {
@@ -249,128 +197,123 @@ submitCheckoutForm() {
       }
       this.sortBy = s;
     },
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//     //Computing lessons information for submitting an order
-//     computeLessonsForOrder() {
-//       this.cart.forEach((i) => {
-//         this.lessonsIDs.push(i);
-//         this.orderLessonSpaces = this.orderLessonSpaces + 1;
-//       });
-//     },
-//   },
+    computed: {
+      totalItemsInTheCart: function () {
+        return this.cart.length || " ";
+      },
 
-  computed: {
-    totalItemsInTheCart: function () {
-      return this.cart.length || " ";
-    },
+      // Computed properties can't have input parameters !!!
 
-    // Computed properties can't have input parameters !!!
+      cartItemCount() {
+        return this.cart.length || "";
+      },
 
-    cartItemCount() {
-      return this.cart.length || "";
-    },
+      // validation computed properties
+      firstNameIsValid() {
+        return !!this.order.firstName;
+      },
 
-    // validation computed properties
-    firstNameIsValid() {
-      return !!this.order.firstName;
-    },
+      phoneNumberIsValid() {
+        return !!this.order.phoneNumber;
+      },
 
-    phoneNumberIsValid() {
-      return !!this.order.phoneNumber;
-    },
+      formIsValid() {
+        return this.firstNameIsValid && this.phoneNumberIsValid;
+      },
 
-    formIsValid() {
-      return this.firstNameIsValid && this.phoneNumberIsValid;
-    },
-			
-		// Retrieve Corresponding Item ID from a Basket - EXPERIMENT FUNCTION !!!
-    retrieveCartItem(id) {
-       var item = this.cart.find(x => x.id == id);
-       return item;
-    },
+      // Retrieve Corresponding Item ID from a Basket - EXPERIMENT FUNCTION !!!
+      retrieveCartItem(id) {
+        var item = this.cart.find((x) => x.id == id);
+        return item;
+      },
 
-    //----------------------------------------------------------------------------------------------------------------------------------------
-    // TESTING                                                                                                                               |
-    //----------------------------------------------------------------------------------------------------------------------------------------
+      //----------------------------------------------------------------------------------------------------------------------------------------
+      // TESTING                                                                                                                               |
+      //----------------------------------------------------------------------------------------------------------------------------------------
 
-    // 2nd approach
-    filteredClasses() {
-      if (this.products) {
-        let tempClasses = this.products;
+      // 2nd approach
+      filteredClasses() {
+        if (this.products) {
+          let tempClasses = this.products;
 
-        // Sort by alphabetical Order
-        // sort value has to change in order to be able to swap between different sorting techniques !!!
-        tempClasses = tempClasses.sort((a, b) => {
-          if (this.sortValue == "subject") {
-            let fa = a.subject.toLowerCase(),
-              fb = b.subject.toLowerCase();
+          // Sort by alphabetical Order
+          // sort value has to change in order to be able to swap between different sorting techniques !!!
+          tempClasses = tempClasses.sort((a, b) => {
+            if (this.sortValue == "subject") {
+              let fa = a.subject.toLowerCase(),
+                fb = b.subject.toLowerCase();
 
-            if (fa < fb) {
-              return -1;
+              if (fa < fb) {
+                return -1;
+              }
+              if (fa > fb) {
+                return 1;
+              }
+
+              return 0;
+            } else if (this.sortValue == "location") {
+              let fa = a.location.toLowerCase(),
+                fb = b.location.toLowerCase();
+
+              if (fa < fb) {
+                return -1;
+              }
+              if (fa > fb) {
+                return 1;
+              }
+            } else if (this.sortValue == "price") {
+              let fa = a.price,
+                fb = b.price;
+
+              if (fa < fb) {
+                return -1;
+              }
+              if (fa > fb) {
+                return 1;
+              }
+            } else if (this.sortValue == "availability") {
+              let fa = a.numberOfSpaces,
+                fb = b.numberOfSpaces;
+
+              if (fa < fb) {
+                return -1;
+              }
+              if (fa > fb) {
+                return 1;
+              }
             }
-            if (fa > fb) {
-              return 1;
-            }
+          });
 
-            return 0;
-          } else if (this.sortValue == "location") {
-            let fa = a.location.toLowerCase(),
-              fb = b.location.toLowerCase();
-
-            if (fa < fb) {
-              return -1;
-            }
-            if (fa > fb) {
-              return 1;
-            }
-          } else if (this.sortValue == "price") {
-            let fa = a.price,
-              fb = b.price;
-
-            if (fa < fb) {
-              return -1;
-            }
-            if (fa > fb) {
-              return 1;
-            }
-          } else if (this.sortValue == "availability") {
-            let fa = a.numberOfSpaces,
-              fb = b.numberOfSpaces;
-
-            if (fa < fb) {
-              return -1;
-            }
-            if (fa > fb) {
-              return 1;
-            }
+          // Show sorted array in descending or ascending order
+          if (!this.ascending) {
+            tempClasses.reverse();
           }
-        });
-
-        // Show sorted array in descending or ascending order
-        if (!this.ascending) {
-          tempClasses.reverse();
+          return tempClasses;
         }
-        return tempClasses;
-      }
-    },
+      },
 
-    // implement the toLowerCase() so that the search can be used with the both small and capital letters
-    filteredClassesBar() {
-      return this.products.filter((product) => {
-        return (
-          product.subject.toLowerCase().match(this.search.toLowerCase()) ||
-          product.location.toLowerCase().match(this.search.toLowerCase()) ||
-          product.description.toLowerCase().match(this.search.toLowerCase())
-        ); // Not sure whether this is correct logic to search in search bar for a price? || product.price.match(this.search) || product.numberOfSpaces.match(this.search)
-        console.log("I was here!");
-        // || product.numberOfSpaces.match(parseInt(this.search))
-      });
-    },
+      // implement the toLowerCase() so that the search can be used with the both small and capital letters
+      filteredClassesBar() {
+        return this.products.filter((product) => {
+          return (
+            product.subject.toLowerCase().match(this.search.toLowerCase()) ||
+            product.location.toLowerCase().match(this.search.toLowerCase()) ||
+            product.description.toLowerCase().match(this.search.toLowerCase())
+          ); // Not sure whether this is correct logic to search in search bar for a price? || product.price.match(this.search) || product.numberOfSpaces.match(this.search)
+          console.log("I was here!");
+          // || product.numberOfSpaces.match(parseInt(this.search))
+        });
+      },
 
-    basketClasses() {
-      if (product.numberOfSpaces > 0) {
-        return product;
-      }
+      basketClasses() {
+        if (product.numberOfSpaces > 0) {
+          return product;
+        }
+      },
+
+      ///////////////////////////////////////////////////////////////THE END OF THE FILE///////////////////////////////////////////////////////////////
     },
   },
 });
