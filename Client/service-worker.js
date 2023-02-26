6dc819780600e62c6cf2d16789ef3f36f7dbc2df
wiktorklusek <63818570+wiktorklusek@users.noreply.github.com>
2023-02-26 22:40:29 +0000

    Update service-worker.js

--- Client/service-worker.js
+++ Client/service-worker.js
@@ -19,14 +19,14 @@ var cacheFiles = [
   'assets/robot.svg',
 ];
 
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      console.log('[Service Worker] Caching all the files');
-      return cache.addAll(cacheFiles);
-    })
-   );
+self.addEventListener("install", function (e) {
+    console.log("[Service Worker] Install");
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log("[Service Worker] Caching all the files");
+            return cache.addAll(cacheFiles);
+        })
+    );
 });
 
 self.addEventListener('fetch', function (e) {
