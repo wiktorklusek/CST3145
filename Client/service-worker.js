68db1289ed9e42885ae11b19461f696190072d18
wiktorklusek <63818570+wiktorklusek@users.noreply.github.com>
2023-02-26 22:06:14 +0000

    Create service-worker.js

---
+++ Client/service-worker.js
@@ -0,0 +1,20 @@
+var cacheName = 'webstore-v1';
+var cacheFiles = [
+  'index.html',
+  'app.js',
+  'css/style.css',
+  'product.js',
+  'webstore.manifest',
+  'assets/art.svg',
+  'assets/chemistry.svg',
+  'assets/dna.svg',
+  'assets/earth.svg',
+  'assets/economy.svg',
+  'assets/icon-bigger.svg',
+  'assets/icon-smaller.svg',
+  'assets/it.svg',
+  'assets/language.svg',
+  'assets/math.svg',
+  'assets/poetry.svg',
+  'assets/robot.svg',
+];
