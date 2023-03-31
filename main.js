232ef7daca5c90fad82971cabe211e40084ac197
wiktorklusek <63818570+wiktorklusek@users.noreply.github.com>
2023-03-31 14:11:50 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import './assets/main.css'
-import './assets/mystyles.css'
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faShoppingCart, faTextHeight, faSave, faTrash, faSync } from '@fortawesome/free-solid-svg-icons'
-import { faUniregistry } from '@fortawesome/free-brands-svg-icons'
-
-library.add(faShoppingCart, faTextHeight, faSave, faTrash, faSync, faUniregistry)
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-
-Vue.config.productionTip = false
+import "./style.css";
 
 new Vue({
-  render: (h) => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
