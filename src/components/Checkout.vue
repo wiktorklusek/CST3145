42ccae3e7b66ee08cb2b622bf11eb798db2926ec
wiktorklusek <63818570+wiktorklusek@users.noreply.github.com>
2023-03-31 14:11:31 +0100

    Add files via upload

---
+++ src/components/Checkout.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div class="row">
+      <!-- Products DIVs -->
+      <div class="col-sm-6 col-md-6 col-lg-4 mt-2" v-for="item in cart">
+        <div class="card card-width rounded-0 shadow-sm">
+          <div class="card-body d-flex justify-content-between">
+            <div>
+              <span class="card-text fs-14">Subject:
+                <b class="card-subtitle mb-2 fs-14">{{ item.lesson.subject }}</b>
+              </span>
+              <br />
+              <span class="card-text fs-14">Location:
+                <b class="card-subtitle mb-2 fs-14">{{ item.lesson.location }}</b>
+              </span>
+              <br />
+              <span class="card-text fs-14">Price:
+                <b class="card-subtitle mb-2 fs-14">£{{ item.lesson.price }}</b>
+              </span>
+              <br />
+              <span class="card-text fs-14">Spaces:
+                <b class="card-subtitle mb-2 fs-14">{{ item.numberOfSpaces }}</b>
+              </span>
+              <br />
+              <span class="card-text fs-18"><i v-bind:class="item.lesson.icon"></i></span><br />
+                        <button @click="removeFromCart(item.lessonId)" class="card-link btn btn-dark btn-sm mt-md-5 px-lg-2">
+            <i class="fas fa-minus mx-1"></i> Remove
+          </button>
+        </div>
+        <img v-bind:src="item.lesson.image" v-bind:alt="item.lesson.altText" class="img-fluid border rounded"
+          style="height: 300px; width: 300px;" />
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Checkout DIVs -->
+<hr>
+<p class="text-center mt-5">Checkout</p>
+<form>
+  <div class="row justify-content-center">
+    <div class="col-md-6 col-lg-4">
+      <input type="text" class="form-control" placeholder="Enter name">
+    </div>
+    <div class="col-md-6 col-lg-4">
+      <input type="number" class="form-control" placeholder="Enter phone number">
+    </div>
+  </div>
+</form>
+<div class="row mt-5 text-center">
+  <div class="col-12">
+    <button v-on:click="showConfirmationDialog" class="card-link btn btn-dark btn-sm px-3">
+      <i class="fas fa-check mx-2"></i> Checkout
+    </button>
+  </div>
+</div>
+ </div>
+</template>
+
+<script>
+export default {
+    name: "Checkout",
+    props: ["cart"],
+    methods: {
+        /**
+         * Emits a custom event that triggers the parent component to remove a lesson from cart
+         * @param {string} _id - The id of the lesson to be removed from the cart
+         */
+        removeFromCart: function (_id) {
+            this.$emit("remove-from-cart", _id);
+        },
+        /**
+         * Displays a confirmation dialog after the user has submitted their order
+         * and reloads the page.
+         */
+        showConfirmationDialog(){
+            alert('Order has been submitted successfully')
+            location.reload();
+        }
+    }
+}
+</script>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="col-md-10">
+    <div class="row">
+      <div class="col-sm-6 col-md-6 col-lg-4 mt-2" v-for="lesson in lessonList">
+        <div class="card card-width rounded-0 shadow-sm">
+          <div class="card-body d-flex justify-content-between">
+            <div>
+              <span class="card-text fs-14">
+                Subject: <b class="card-subtitle mb-2 fs-14">{{ lesson.subject }}</b>
+              </span><br>
+              <span class="card-text fs-14">
+                Location: <b class="card-subtitle mb-2 fs-14">{{ lesson.location }}</b>
+              </span><br>
+              <span class="card-text fs-14">
+                Price: <b class="card-subtitle mb-2 fs-14">£{{ lesson.price }}</b>
+              </span><br>
+              <span class="card-text fs-14">
+                Spaces: <b class="card-subtitle mb-2 fs-14">{{ lesson.numberOfSpaces }}</b>
+              </span><br>
+              <span class="card-text fs-18">
+                <i v-bind:class="lesson.icon"></i>
+              </span><br>
+              <button v-bind:class="{ disabled: lesson.numberOfSpaces <= 0 }"
+                @click="addToCart(lesson._id)" class="card-link btn btn-dark btn-sm mt-md-5 px-lg-2">
+                <i class="fas fa-plus mx-1"></i>
+                <!-- <span class="d-xl-none">Add</span> -->
+                <span class="d-none d-xl-inline">Add to cart</span>
+              </button>
+            </div>
+            <br>
+            <img v-bind:src="lesson.image" class="img-fluid border rounded"
+              style="height: 300px; width: 300px;">
+              <br>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+    // Defines the name of the component.
+    name: "ProductList",
+    // Declares props that are passed to this component from the parent component.
+    props: ["lessonList", "search"],
+    methods: {
+        // This method is called when the "Add to cart" button is clicked. It emits a custom event "add-to-cart" with the ID of the lesson as the argument, which triggers the parent component to add the selected lesson to cart.
+        addToCart: function (_id) {
+            this.$emit("add-to-cart", _id);
+        }
+    }
+}
+</script>
